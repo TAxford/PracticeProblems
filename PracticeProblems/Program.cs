cf39002f8a5d67a4439b0bf721a330064b7b5a25
TAxford <t.axford2@yahoo.com>
2018-01-23 12:58:04 -0500

    Test

---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Day 1
+            //string fullName = "My Full Name is Timothy Joe Axford II";
+
+            //Console.WriteLine(fullName);
+
+            //Console.WriteLine("");
+
+            //string favCar = "My Favorite car is a 1965 Shelby Cobra GT 500";
+
+            //Console.WriteLine(favCar);
+
+            //Console.WriteLine("");
+
+            //string favHero = "My Favorite Comic Superhero is Batman";
+
+            //Console.WriteLine(favHero);
+
+            //Console.WriteLine("");
+
+            //Day 2
+
+            double valueOne = 7;
+            double valueTwo = 2;
+            double remainder = valueOne - valueTwo;
+
+            Console.WriteLine("Enter you name.");
+            string response = Console.ReadLine();
+
+            Console.WriteLine("He or She.");
+            string sex = Console.ReadLine();
+
+            Console.WriteLine((response) + " needs to use {0} cups of sugar. " + (sex) + " had used {1} cups. Therefore " + (sex) + " needs another {2} cups", valueOne, valueTwo, remainder);
+
+            //Console.WriteLine(response "needs to use {0} cups of Sugar. She has used {1} already. This leaves a total remaining of (valueOne - valueTwo); valueOne, valueTwo
+
+
+
+            //valueOne = 13;
+            //valueTwo = 4;
+            //string personOne = "Mike";
+            //string personTwo = "Jake";
+            //string persontThree = "Bob";
+            //string personFour = "Kimberly";
+
+            //Console.WriteLine("Mike" + personTwo + persontThree + personFour + "had a bill totaling" + (valueOne * valueTwo));
+
+            //            valueOne = 40;
+            //            valueTwo = 8;
+            //            Console.WriteLine(You can buy
+
+            //            valueOne = 41;
+            //            valueTwo = 29;
+            //            Console.WriteLine(valueOne - valueTwo);
+
+            //            valueOne = 47;
+            //            valueTwo = 30;
+            //            Console.WriteLine(valueOne - valueTwo);
+
+            //            valueOne = 12;
+            //            valueTwo = 3;
+            //            Console.WriteLine(valueOne / valueTwo);
+
+            //            valueOne = 27.10;
+            //            valueTwo = 5.12;
+            //            Console.WriteLine(valueOne + valueTwo);
+
+            //            //examples of magic numbers dont use
+
+
+
+            //            //should looke like valueOne and ValueTwo
+
+            //            //int valueOne = 7;
+            //            //int valueTwo = 2;
+            //            //Console.WriteLine(valueOne - valueTwo);
+
+            //            //Day 3
+
+            //            
+            //            //-How many packages of diapers can you buy with $40 if one package costs $8 ?
+            //            //-Last Friday Trevon had $29.Over the weekend he received some money for cleaning the attic.He now has $41.How much money did he receive ?
+            //            //-Last week Julia ran 30 miles more than Pranav.Julia ran 47 miles.How many miles did Pranav run ?
+            //            //-How many boxes of envelopes can you buy with $12 if one box costs $3 ?
+            //            //-After paying $5.12 for a salad, Norachai has $27.10.How much money did he have before buying the salad ?
+        }
+    }
+}
